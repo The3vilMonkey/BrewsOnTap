b8203ce57f9df525348ccd07a4efb4e9d60c83b6
George Kellerman <funkyeah@gmail.com>
2014-11-10 19:51:18 -0600

    adding launchScreens

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,9 @@
 App.info({
   name: 'Tin Whiskers',
   description: 'An app to track if your favorite brews are curruently on tap',
-  version: '0.0.2'
+  version: '0.0.2',
+  email: 'george@twbrewing.com',
+  website: 'http://twbrewing.com'
 });
 
 App.icons({
@@ -9,4 +11,10 @@ App.icons({
   'android_mdpi': 'icons/icons_48x48.png',
   'android_hdpi': 'icons/icons_72x72.png',
   'android_xhdpi': 'icons/icons_96x96.png'
+});
+
+App.launchScreens({
+  'android_mdpi_portrait' : 'mobile/launchScreens/drawable-mdpi/circular_wordmark.9.png',
+  'android_hdpi_portrait' : 'mobile/launchScreens/drawable-ldpi/circular_wordmark.9.png',
+  'android_xhdpi_portrait' : 'mobile/launchScreens/drawable-xhdpi/circular_wordmark.9.png'
 });
