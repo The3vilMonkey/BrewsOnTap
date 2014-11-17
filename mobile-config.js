0a986530e3666d74d08f444561ca181620ed9259
The3vilMonkey <cskellerman@gmail.com>
2014-11-16 18:35:44 -0600

    Changed app name to Tin Whiskers and added icons folder.

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,12 @@
 App.info({
-  name: 'BrewsOnTap',
+  name: 'Tin Whiskers',
   description: 'An app to track if your favorite brews are curruently on tap',
   version: '0.0.1'
 });
 
 App.icons({
-  'android_ldpi': 'resources/icons/icon-ldpi.png',
-  'android_mdpi': 'resources/icons/icon-mdpi.png',
-  'android_hdpi': 'resources/icons/icon-hdpi.png',
-  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+  'android_ldpi': 'icons/icons_36x36.png',
+  'android_mdpi': 'icons/icons_48x48.png',
+  'android_hdpi': 'icons/icons_72x72.png',
+  'android_xhdpi': 'icons/icons_96x96.png'
 });
