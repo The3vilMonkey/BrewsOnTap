aeefd90158450cf59efcbb8b678de678cd3118bf
George Kellerman <funkyeah@gmail.com>
2014-11-17 16:04:01 -0600

    regular pngs

--- mobile-config.js
+++ mobile-config.js
@@ -14,7 +14,8 @@ App.icons({
 });
 
 App.launchScreens({
-  'android_mdpi_portrait' : 'mobile/launchScreens/drawable-mdpi/circular_wordmark.9.png',
-  'android_hdpi_portrait' : 'mobile/launchScreens/drawable-hdpi/circular_wordmark.9.png',
-  'android_xhdpi_portrait' : 'mobile/launchScreens/drawable-xhdpi/circular_wordmark.9.png'
+  'android_ldpi_portrait' : 'mobile/launchScreens/drawable-ldpi/circle_wordmark_200x320.png',
+  'android_mdpi_portrait' : 'mobile/launchScreens/drawable-mdpi/circle_wordmark_320x480.png',
+  'android_hdpi_portrait' : 'mobile/launchScreens/drawable-hdpi/circle_wordmark_480x800.png',
+  'android_xhdpi_portrait' : 'mobile/launchScreens/drawable-xhdpi/circle_wordmark_720x1280.png'
 });
