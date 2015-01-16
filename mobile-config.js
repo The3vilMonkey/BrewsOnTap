58c04e6084b0898bebac7443e9e4e9ba18fc6a51
George Kellerman <funkyeah@gmail.com>
2015-01-16 12:55:22 -0600

    debugging push test

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 App.info({
   name: 'Tin Whiskers',
   description: 'An app to track if your favorite brews are currently on tap',
-  version: '0.0.7',
+  version: '0.0.8',
   email: 'george@twbrewing.com',
   website: 'http://twbrewing.com'
 });
